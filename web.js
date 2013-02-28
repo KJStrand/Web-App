5b3977c137d9b3017536e338bfb02225f7ebecdd
KJStrand <trovius@gmail.com>
2013-02-27 21:44:56 -0700

    init

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello Kaden!');
-  response.send(document.write("<p>My First JavaScript</p>");
+
 });
 
 
