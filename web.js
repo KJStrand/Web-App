33fb4418ebdd0102f30452b9ac5ecca596c2759c
KJStrand <trovius@gmail.com>
2013-02-27 21:40:11 -0700

    init

--- web.js
+++ web.js
@@ -1,16 +1,21 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello Kaden!');
-  response.sent(document.write("<p>My First JavaScript</p>"));
-});
-
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
-
-
+var http = require('http');
+var fs = require('fs');
+ 
+http.createServer(function (request, response) {
+ 
+    console.log('request starting...');
+     
+    fs.readFile('./static.html', function(error, content) {
+        if (error) {
+            response.writeHead(500);
+            response.end();
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.end(content, 'utf-8');
+        }
+    });
+     
+}).listen(8125);
+ 
+console.log('Server running at http://127.0.0.1:8125/');
