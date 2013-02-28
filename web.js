1704c95a3a383ac26230fe0217d6149ce83aa0d6
KJStrand <trovius@gmail.com>
2013-02-27 20:54:41 -0700

    init

--- web.js
+++ web.js
@@ -6,6 +6,8 @@ app.get('/', function(request, response) {
   response.send('Hello Kaden!');
 });
 
+document.write("<p>My First JavaScript</p>");
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
