f3188d2541c1f89e537f8b427146a533f1ff3256
KJStrand <trovius@gmail.com>
2013-02-27 21:15:13 -0700

    init

--- web.js
+++ web.js
@@ -4,6 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello Kaden!');
+  document.write("<p>My First JavaScript</p>");
 });
 
 
@@ -13,4 +14,3 @@ app.listen(port, function() {
 });
 
 
-document.write("<p>My First JavaScript</p>");
