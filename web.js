25037222814d84c7854b89fea140bd28dbbdac19
KJStrand <trovius@gmail.com>
2013-02-27 22:08:18 -0700

    init

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   
-  fs.readFile('./index.htm', function(error, content) {
+  fs.readFile('./static.html', function(error, content) {
         if (error) {
             response.writeHead(500);
             response.end();
