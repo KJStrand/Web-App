1086994f5144cd01d7ddc1aa85127547e12e7fe9
KJStrand <trovius@gmail.com>
2013-03-04 10:49:11 -0700

    init

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + "/public"));
 
 app.get('/', function(request, response) {
   
-  fs.readFile('./Untitled-5.html', function(error, content) {
+  fs.readFile('./app.html', function(error, content) {
         if (error) {
             response.writeHead(500);
             response.end();
